53571ffd34fcc548a50f236fc4dac3b65d283bd6
aaroncallagher <me@aaroncallagher.com>
2016-08-18 21:33:04 +0000

    Added Exercise 7 Solution

---
+++ lesson_3/easy_1/question_7.rb
@@ -0,0 +1,14 @@
+def add_eight(number)
+  number + 8
+end
+
+number = 2
+
+how_deep = "number"
+5.times { how_deep.gsub!("number", "add_eight(number)") }
+
+p how_deep
+
+p eval(how_deep)
+
+# 42 = 2 + 8 + 8 + 8 + 8 + 8
