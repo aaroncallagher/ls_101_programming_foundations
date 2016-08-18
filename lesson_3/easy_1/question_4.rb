e2b35c6a890bcc156c190cc7220fe974f986fa49
aaroncallagher <me@aaroncallagher.com>
2016-08-18 21:01:03 +0000

    Added Exercise 4 Solution

---
+++ lesson_3/easy_1/question_4.rb
@@ -0,0 +1,11 @@
+numbers = [1,2,3,4,5]
+numbers.delete_at(1)
+
+# delete_at will delete the element at the specified index, and will return
+#  that element. In this case, it will reurn 2
+
+numbers = [1,2,3,4,5]
+numbers.delete(1)
+
+# delete will delete all items matching the value of the parameter passed in.
+#  returns the deleted item. In this case, it will delete the value 1 and return
