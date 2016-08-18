769b55455918d9124829e2f6182a0dbec442d71a
aaroncallagher <me@aaroncallagher.com>
2016-08-18 21:18:25 +0000

    Added Question 6 Solution

---
+++ lesson_3/easy_1/question_6.rb
@@ -0,0 +1,10 @@
+famous_words = "seven years ago..."
+
+# The first way would be to add it manually.
+famous_words = "Four score and " + famous_words
+p famous_words
+
+famous_words = "seven years ago..."
+
+# the second way would be to use the prepend method
+famous_words.prepend("Four score and ")
