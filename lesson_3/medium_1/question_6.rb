591d21bfc2b6605d4d2288e7e927f849914ab640
aaroncallagher <me@aaroncallagher.com>
2016-08-22 21:08:03 +0000

    Added Question 6 Solution

---
+++ lesson_3/medium_1/question_6.rb
@@ -0,0 +1,15 @@
+def rolling_buffer1(buffer, max_buffer_size, new_element)
+  buffer << new_element
+  buffer.shift if buffer.size >= max_buffer_size
+  buffer
+end
+
+def rolling_buffer2(input_array, max_buffer_size, new_element)
+  buffer = input_array + [new_element]
+  buffer.shift if buffer.size >= max_buffer_size
+  buffer
+end
+
+# I think I would prefer rolling_buffer2, because << mutates the caller, so,
+#  the buffer parameter would be affected by the destructive method, causing
+#  unknown side effects
