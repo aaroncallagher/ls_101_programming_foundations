4ce15b550ad957fac96f2ad91bf227369a3efdc2
aaroncallagher <me@aaroncallagher.com>
2016-08-22 23:29:33 +0000

    Added Question 2 Solution

---
+++ lesson_3/medium_2/question_2.rb
@@ -0,0 +1,9 @@
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+
+munsters.each {|key, value| puts "#{key} is a #{value["age"]} year old #{value["gender"]}"}
