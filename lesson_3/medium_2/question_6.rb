f44d21ceac1b9368b402a0f43078ec03e028d40a
aaroncallagher <me@aaroncallagher.com>
2016-08-23 17:06:03 +0000

    Added Question 6 Solution

---
+++ lesson_3/medium_2/question_6.rb
@@ -0,0 +1,21 @@
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+
+def mess_with_demographics(demo_hash)
+  demo_hash.values.each do |family_member|
+    family_member["age"] += 42
+    family_member["gender"] = "other"
+  end
+end
+
+mess_with_demographics(munsters)
+
+p munsters
+
+# This will affect the data, because the parameter passed in is by reference,
+# and not by value. So, the munsters hash is mutated. 
