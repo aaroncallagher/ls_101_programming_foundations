f4726262dcab45bb46e6c4ded2fc83dd49f89243
aaroncallagher <me@aaroncallagher.com>
2016-08-24 18:00:55 +0000

    Added Question 3 Solution

---
+++ lesson_3/hard_1/question_3.rb
@@ -0,0 +1,64 @@
+# one is: one
+# two is: two
+# three is: three
+
+# Because, the method re-assigns new string objects inside the method scope, but
+# doesnt affect the outer scope
+
+def mess_with_vars(one, two, three)
+  one = two
+  two = three
+  three = one
+end
+
+one = "one"
+two = "two"
+three = "three"
+
+mess_with_vars(one, two, three)
+
+puts "one is: #{one}"
+puts "two is: #{two}"
+puts "three is: #{three}"
+
+# This will print the same as before for the same reasons
+
+def mess_with_vars(one, two, three)
+  one = "two"
+  two = "three"
+  three = "one"
+end
+
+one = "one"
+two = "two"
+three = "three"
+
+mess_with_vars(one, two, three)
+
+puts "one is: #{one}"
+puts "two is: #{two}"
+puts "three is: #{three}"
+
+# This will print:
+
+# one is: two
+# two is: three
+# three is: one
+
+# Because gsub! mutates the caller
+
+def mess_with_vars(one, two, three)
+  one.gsub!("one","two")
+  two.gsub!("two","three")
+  three.gsub!("three","one")
+end
+
+one = "one"
+two = "two"
+three = "three"
+
+mess_with_vars(one, two, three)
+
+puts "one is: #{one}"
+puts "two is: #{two}"
+puts "three is: #{three}"
