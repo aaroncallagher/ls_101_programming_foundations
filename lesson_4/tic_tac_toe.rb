16add468b6245c24b2577ec0450b5bdc9adc3622
aaroncallagher <me@aaroncallagher.com>
2016-08-25 22:06:46 +0000

    Tic Tac Toe Game Logic

--- lesson_4/tic_tac_toe.rb
+++ lesson_4/tic_tac_toe.rb
@@ -1,4 +1,4 @@
-
+require 'pry'
 
 INITIAL_MARKER = ' '
 PLAYER_MARKER = 'X'
@@ -9,6 +9,8 @@ def prompt(msg)
 end
 
 def display_board(brd)
+  system 'clear'
+  puts "You are #{PLAYER_MARKER}. Computer is #{COMPUTER_MARKER}"
   puts ""
   puts "     |     |"
   puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}  "
@@ -42,12 +44,64 @@ def player_places_piece!(brd)
     break if empty_squares(brd).include?(square)
     prompt("Sorry, that is an invalid choice.")
   end
-  
   brd[square] = PLAYER_MARKER
 end
 
-board = initialize_board
-display_board(board)
+def computer_places_piece!(brd)
+  square = empty_squares(brd).sample
+  brd[square] = COMPUTER_MARKER
+end
 
-player_places_piece!(board)
-display_board(board)
+def board_full?(brd)
+  empty_squares(brd).empty?
+end
+
+def someone_won?(brd)
+  !!detect_winner(brd)
+end
+
+def detect_winner(brd)
+  winning_lines = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
+                  [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + #columns
+                  [[1, 5, 9], [3, 5, 7]] # diagonals
+  
+  winning_lines.each do |line|
+    if brd[line[0]] == PLAYER_MARKER &&
+       brd[line[1]] == PLAYER_MARKER &&
+       brd[line[2]] == PLAYER_MARKER
+       return "You"
+    elsif brd[line[0]] == COMPUTER_MARKER &&
+          brd[line[1]] == COMPUTER_MARKER &&
+          brd[line[2]] == COMPUTER_MARKER
+          return "Computer"
+    end
+  end
+  nil
+end
+
+loop do
+  board = initialize_board
+  
+  loop do
+    display_board(board)
+    
+    player_places_piece!(board)
+    break if someone_won?(board) || board_full?(board)
+    
+    computer_places_piece!(board)
+    break if someone_won?(board) || board_full?(board)
+  end
+  
+  display_board(board)
+  
+  if someone_won?(board)
+    prompt("#{detect_winner(board)} won!")
+  else
+    prompt("Cat's Game!")
+  end
+  prompt("Play Again? (y or n)")
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
+end
+
+prompt("Thanks for playing Tic-Tac-Toe. Good-Bye")
