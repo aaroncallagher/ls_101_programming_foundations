5059853d5d66acd6a62d73d1534d8a24790a956d
aaroncallagher <me@aaroncallagher.com>
2016-09-05 19:20:57 +0000

    Added Final Tic Tac Toe Bonus Features

--- lesson_4/tictactoe_bonus_features.rb
+++ lesson_4/tictactoe_bonus_features.rb
@@ -1,5 +1,7 @@
 require 'pry'
 
+GO_FIRST = 'choose'.freeze
+
 WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
                 [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # columns
                 [[1, 5, 9], [3, 5, 7]] # diagonals
@@ -52,11 +54,10 @@ def player_places_piece!(brd)
   brd[square] = PLAYER_MARKER
 end
 
-def computer_ai_moves(line, board, marker)
-  if board.values_at(*line).count(marker) == 2
-    board.select{|key, val| line.include?(key) && val == INITIAL_MARKER}.keys.first
-  else
-    nil
+def computer_ai_moves(line, brd, marker)
+  if brd.values_at(*line).count(marker) == 2 &&
+     brd.values_at(*line).count(INITIAL_MARKER) == 1
+    line[brd.values_at(*line).index(INITIAL_MARKER)]
   end
 end
 
@@ -66,27 +67,39 @@ def computer_places_piece!(brd)
     square = computer_ai_moves(line, brd, COMPUTER_MARKER)
     break if square
   end
-  
-  if !square
-    WINNING_LINES.each do |line|
-      square = computer_ai_moves(line, brd, PLAYER_MARKER)
-      break if square
-    end
+
+  WINNING_LINES.each do |line|
+    square ||= computer_ai_moves(line, brd, PLAYER_MARKER)
+    break if square
   end
-  
-  if !square
-    if brd[5] == INITIAL_MARKER
-      square = 5
-    else
-      square = empty_squares(brd).sample
-    end
-  end
-  
+
+  square ||= 5 if brd[5] == INITIAL_MARKER
+  square ||= empty_squares(brd).sample
   brd[square] = COMPUTER_MARKER
+end
+
+def place_piece!(brd, player)
+  if player == 'you'
+    player_places_piece!(brd)
+  else
+    computer_places_piece!(brd)
+  end
+end
+
+def alternate_player(player)
+  if player == "you"
+    "computer"
+  else
+    "you"
+  end
 end
 
 def board_full?(brd)
   empty_squares(brd).empty?
+end
+
+def display_score(player_score, computer_score)
+  prompt("You: #{player_score} || Computer: #{computer_score}")
 end
 
 def someone_won?(brd)
@@ -110,24 +123,36 @@ def joinor(choices, punctuation = ',', conjunction = 'or')
     if choices.length == 1
       str = choice
     elsif index == (choices.length - 1)
-      str += "#{conjunction}" + " " + "#{choice}"
+      str += conjunction.to_s + " " + choice.to_s
     else
-      str += "#{choice}" + "#{punctuation}" + " "
+      str += choice.to_s + punctuation.to_s + " "
     end
   end
   str
 end
 
+player_victories = 0
+computer_victories = 0
+
 loop do
   board = initialize_board
+  if GO_FIRST == "choose"
+    player = ''
+    loop do
+      prompt "Who will start the Game? (You or Computer):"
+      player = gets.chomp.downcase
+      break if %w(you computer).include?(player)
+      prompt "Invalid choice."
+    end
+  else
+    player = GO_FIRST
+  end
 
   loop do
     display_board(board)
-
-    player_places_piece!(board)
-    break if someone_won?(board) || board_full?(board)
-
-    computer_places_piece!(board)
+    place_piece!(board, player)
+    display_board(board)
+    player = alternate_player(player)
     break if someone_won?(board) || board_full?(board)
   end
 
@@ -135,12 +160,23 @@ loop do
 
   if someone_won?(board)
     prompt("#{detect_winner(board)} won!")
+    if detect_winner(board) == 'You'
+      player_victories += 1
+    else
+      computer_victories += 1
+    end
   else
     prompt("Cat's Game!")
   end
-  prompt("Play Again? (y or n)")
-  answer = gets.chomp
-  break unless answer.downcase.start_with?('y')
+
+  display_score(player_victories, computer_victories)
+  if player_victories == 5
+    prompt("****YOU WIN!!!!****")
+    break
+  elsif computer_victories == 5
+    prompt("The computer won :( Better luck next time")
+    break
+  end
 end
 
 prompt("Thanks for playing Tic-Tac-Toe. Good-Bye")
